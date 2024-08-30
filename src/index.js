a5a6fdc5e348599760a76d4f9e0e1813d83c128b
isaacbatista26 <isaacbatista2266@gmail.com>
2024-08-30 12:02:30 -0300

    feat: add new health endpoint

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const port = 5000;
 const citiesRoutes = require('./routes/citiesRoutes');
 const tendersMonthRoutes = require('./routes/tendersMonthRoutes');
 const tendersYearRoutes = require('./routes/tendersYearRoutes');
-const tendersRoutes = require('./routes/tendersRoutes');
 const unitsRoutes = require('./routes/unitsRoutes');
+const healthRoutes = require('./routes/healthRoutes');
 
 app.use(cors())
 
@@ -16,6 +16,7 @@ app.use('/cities', citiesRoutes)
 app.use('/tenders', tendersMonthRoutes)
 app.use('/tenders/year', tendersYearRoutes)
 app.use('/units', unitsRoutes)
+app.use('/health', healthRoutes)
 
 app.listen(port, () => {
   console.log(`API running at http://localhost:${port}`)
