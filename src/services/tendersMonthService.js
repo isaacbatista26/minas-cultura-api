c00d151a389bb31eecf49680083669bd35e3d2e1
isaacbatista26 <isaacbatista2266@gmail.com>
2024-09-02 22:47:33 -0300

    fix: Consertando novamente o tenders Month

--- src/services/tendersMonthService.js
+++ src/services/tendersMonthService.js
@@ -8,16 +8,19 @@ async function getTendersByMonth(start, end) {
 
     let query = supabase.from('tendersmonth').select('*');
 
-    if(startYear === endYear) {
+    if (startYear === endYear) {
+        // Caso em que o ano inicial e final são o mesmo
         query = query
             .eq('year', startYear)
             .gte('month', startMonth)
             .lte('month', endMonth);
     } else {
+        // Caso em que o ano inicial e final são diferentes
         query = query
             .or(
-                `and(year.eq.${startYear},month.gte.${startMonth}),` +
-                `and(year.eq.${endYear},month.lte.${endMonth})`
+                `and(year.eq.${startYear},month.gte.${startMonth}),` +   // Meses do ano inicial
+                `and(year.eq.${endYear},month.lte.${endMonth}),` +      // Meses do ano final
+                `and(year.gt.${startYear},year.lt.${endYear})`          // Todos os anos intermediários
             );
     }
 
@@ -26,7 +29,7 @@ async function getTendersByMonth(start, end) {
     if (error) {
         throw new Error(error.message);
     }
-
+    
     return data;
 }
 
